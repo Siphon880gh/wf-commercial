719ca804400b8f0e6ecda06a0b43810257af8003
Siphon880gh <weng.f.fung@gmail.com>
2025-06-27 23:51:06 -0700

    More DevOps - Base URL

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/me',
+  assetPrefix: isProd ? '/me/' : '', // Only set prefix in production
 };
 
 export default nextConfig;
